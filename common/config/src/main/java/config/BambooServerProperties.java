0f0ce619a234ac10061db0802a83dad80fffc84a
林可珂 <linkk@fadada.com>
2020-10-23 17:49:43 +0800

    xuexi

---
+++ common/config/src/main/java/config/BambooServerProperties.java
@@ -0,0 +1,21 @@
+package config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 配置属性类
+ */
+@ConfigurationProperties(prefix = "bamboo")
+public class BambooServerProperties {
+	private static final String NAME = "bamboo_server0";
+
+	private String name = NAME;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ common/config/src/main/java/config/BmbooServiceAutoConfiguration.java
@@ -0,0 +1,28 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(BambooServerProperties.class)
+@ConditionalOnClass(BambooServer.class)
+@ConditionalOnProperty(prefix = "bamboo",value = "enabled",matchIfMissing = true)
+public class BmbooServiceAutoConfiguration {
+
+	@Autowired
+	private BambooServerProperties mistraServiceProperties;
+
+	@Bean(name = "bambooServer")
+	@ConditionalOnMissingBean(BambooServer.class)
+	public BambooServer mistraService(){
+		BambooServer mistraService = new BambooServer();
+		mistraService.setName(mistraServiceProperties.getName());
+		return mistraService;
+	}
+
+}
